b95096bc11a3a6c188fd5b691e226e26d3ce043a
Framer GitHub Importer <>
2019-12-01 22:01:15 +0100

    Initial commit

---
+++ code/canvas.tsx
@@ -0,0 +1,4 @@
+// WARNING: this file is auto generated, any changes will be lost
+import { createDesignComponent, CanvasStore } from "framer"
+const canvas = CanvasStore.shared(); // CANVAS_DATA;
+export const DesignComponent = createDesignComponent<{parentSize?:{width:number|string,height:number|string},width?:number|string,height?:number|string}>(canvas, "id_I1biqFnSk", {}, 311,190);
